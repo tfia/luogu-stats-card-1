c76bebb1745a8bcac0f8ab62889d60dc00c9afaa
RHDu <35389556+tfia@users.noreply.github.com>
2021-01-26 13:57:08 +0800

    update

--- src/stats-card.js
+++ src/stats-card.js
@@ -83,7 +83,7 @@ const renderSVG = (stats, options) => {
   const passedSum = passed.reduce((a, b) => a + b);
   const body = renderChart(datas, labelWidth, progressWidth, "题");
 
-  const title = renderNameTitle("看看", name, color, ccfLevel, "做题做得怎么样呢？", cardWidth, `已通过: ${passedSum}题`);
+  const title = renderNameTitle(name, color, ccfLevel, "做题做得怎么样呢？", cardWidth, `已通过: ${passedSum}题`);
 
   return new Card({
     width: cardWidth - 2*paddingX,
